7781f37545f7d9facc6ce56fb95c1cde9c0318c7
Meesa Shivaram Prasad <55886366+codemaster-22@users.noreply.github.com>
2020-02-22 23:19:20 +0530

    Create Node.cpp

---
+++ src/Node.cpp
@@ -0,0 +1,8 @@
+ #include "../include/Node.h"
+ Node::Node(int a=0,int b=0,int c=0,int d=0)
+     {
+         processno=a;
+         priorityvalue=b;
+         arrivaltime=c;
+         bursttime=d;
+     }
