07552c54742e26792261b154d9159a7158242cc4
Meesa Shivaram Prasad <55886366+codemaster-22@users.noreply.github.com>
2020-02-22 23:17:26 +0530

    Create Heap.h

---
+++ include/Heap.h
@@ -0,0 +1,20 @@
+#include "Node.h"
+class Heap
+{
+public:
+    Node* executing;
+    Heap();
+    void printpreorder();
+    void printpreorder(int ii);
+    void insert(Node* ptr);
+    Node* returnmin(){ return element[1];}
+    Node* deletemin();
+    int nelements;
+private:
+    vector<Node*> element;
+    int getparent(int ii){return (ii/2);}
+    int getleft(int ii){return (2*ii);}
+    int getright(int ii){return (2*ii)+1;}
+    int percolateup(int ii);
+    int percolatedown(int ii);
+};
