b127a7cffcc419ec1f6c62e6772d3dead123da13
Meesa Shivaram Prasad <55886366+codemaster-22@users.noreply.github.com>
2020-02-22 23:18:17 +0530

    Create Node.h

---
+++ include/Node.h
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Node
+{
+public:
+     int processno;
+     int priorityvalue;
+     int arrivaltime;
+     int bursttime;
+     Node(int a,int b,int c,int d);
+};
